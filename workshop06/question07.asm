d3a3a87513bf61cc73cc8ed3c8ad0b33acf0cfd5
a1778478 <56853075+Crazyorchid@users.noreply.github.com>
2020-09-03 05:13:44 +0000

    questions done

--- workshop06/question07.asm
+++ workshop06/question07.asm
@@ -8,3 +8,46 @@
 	@arr
 // add your instructions after this line
 
+@i
+
+D = 0 
+
+@sum
+
+D = 0
+
+(LOOP)
+
+@i
+
+D = M //D=i
+
+@10
+
+D = D - A // D = i -10
+
+@END
+
+D;JGT 
+
+@i
+
+D = M // D=i
+
+@sum
+
+M = D + M // sum = sum+1
+
+@i
+
+M = M + 1 // x = x+1
+
+@LOOP
+
+0 ; JMP 
+
+(END)
+
+@END
+
+0 ; JMP // Infinite loop
