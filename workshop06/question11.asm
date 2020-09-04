f879a9effed799900d09308ab1be0c0c98dca781
a1778478 <56853075+Crazyorchid@users.noreply.github.com>
2020-09-04 06:40:37 +0000

    q6

--- workshop06/question11.asm
+++ workshop06/question11.asm
@@ -7,4 +7,42 @@
 	@numbers
 	@arr
 // add your instructions after this line
+push rbp
 
+mov rbp, rsp
+
+mov DWORD PTR [rbp-20], edi
+
+mov DWORD PTR [rbp-4], 0
+
+mov DWORD PTR [rbp-8], 0
+
+.L3:
+
+cmp DWORD PTR [rbp-4], 9   #While Condition Checking
+
+jg .L4
+
+mov eax, DWORD PTR [rbp-4]
+
+cdqe
+
+lea rdx, [0+rax*4]
+
+mov rax, QWORD PTR [rbp-16]
+
+add rax, rdx #add the address of number + value of x.
+
+mov eax, DWORD PTR [rax] #Move the value at number[x] to reg eax.
+
+add DWORD PTR [rbp-8], eax #Add the value at *number[x] to sum
+
+add DWORD PTR [rbp-4], 1 #INcrement X .
+
+jmp .L3 #Jump to next iteration.
+
+.L4:
+
+nop
+
+pop rbp
