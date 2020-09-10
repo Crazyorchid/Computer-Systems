deb5f70b827a63a1bf34c489c702b3935b569de9
a1778478 <56853075+Crazyorchid@users.noreply.github.com>
2020-09-10 17:45:38 +0000

    trial

--- assignment1/tokeniser-basics.cpp
+++ assignment1/tokeniser-basics.cpp
@@ -1,5 +1,5 @@
 // a skeleton implementation of a tokeniser
-
+#include <vector>
 #include "tokeniser-extras.h"
 
 // to shorten the code
@@ -12,6 +12,15 @@ namespace Assignment_Tokeniser
 
     // the current input character
     // the eof marker can be confused with a legal character but not one accepted by our tokeniser
+    int line = 0;
+    int column = 0;
+    char for_tab;
+    int column_of_new;
+
+
+    string one_line ;
+    vector<string> all_lines;
+
     char ch ;
 
     // to create a new token we need the characters read since the last token was created
@@ -27,7 +36,55 @@ namespace Assignment_Tokeniser
     Token new_token(TokenKind kind)
     {
         // create a new token object and reset spelling
-        Token token = new_token(kind,spelling,0,0) ;
+        //Token token = new_token(kind,spelling,0,0) ;
+        Token token;
+        if(kind == tk_newline) {
+            if(all_lines.size()>0){
+                if(all_lines.back().back() == '$') {
+                    one_line.back() += '$';
+                    all_lines.push_back(one_line);
+                }
+                else{
+                    all_lines.back() += "$";
+                }
+            }else
+                all_lines.push_back(one_line + "$");
+
+            token = new_token(kind, spelling, line - 1, all_lines.back().size());
+            one_line += "";
+        }else if(kind == tk_adhoc_comment){
+            string copy = spelling;
+            int nl_num = 0;
+
+            for(int i = 0; i < copy.size(); i++){
+                if(copy[i] == '\n') {
+                    string part = copy.substr(0, i);
+                    part += "$";
+                    if(all_lines.back().back() != '$')
+                        all_lines.back()+=part;
+                    else
+                        all_lines.push_back(part);
+
+                    copy = copy.substr(i);
+                    nl_num++;
+                    i = 0;
+
+                }
+                all_lines.push_back(copy);
+            }
+            token = new_token(kind,spelling,line-nl_num,column) ;
+        }
+        else if (kind == tk_eol_comment){
+            string tmp = spelling;
+            tmp.pop_back();
+            tmp += "$";
+            all_lines.push_back(tmp);
+            token = new_token(kind,spelling,line-1,column) ;
+        }else{
+            one_line += spelling;
+            token = new_token(kind,spelling,0,column - spelling.length()) ;
+        }
+
 
         // reset the spelling
         spelling = "" ;
@@ -46,7 +103,57 @@ namespace Assignment_Tokeniser
     // NOTE: it is a good idea to build a data structure to remember where each line starts
     string token_context(Token token)
     {
-        return "" ;
+        string syh = "";
+        string dqh = "";
+        string xyh = "";
+
+        int cur_line = token_line(token);
+        int pre_line = cur_line - 1;
+
+        //leading spaces;
+
+        if(cur_line < 10){
+            dqh += "   " + std::__cxx11::to_string(cur_line) + ": ";
+        }
+        else if(cur_line < 100) {
+            dqh += "  " + std::__cxx11::to_string(cur_line) + ": ";
+        }else if(cur_line < 1000) {
+            dqh += " " + std::__cxx11::to_string(cur_line) + ": ";
+        }else if(cur_line < 1000) {
+            dqh += std::__cxx11::to_string(cur_line) + ": ";
+        }
+
+
+
+        if(cur_line > 1)
+        {
+            if(pre_line < 10){
+                dqh += "   " + std::__cxx11::to_string(pre_line) + ": ";
+            }
+            else if(cur_line < 100) {
+                dqh += "  " + std::__cxx11::to_string(pre_line) + ": ";
+            }else if(cur_line < 1000) {
+                dqh += " " + std::__cxx11::to_string(pre_line) + ": ";
+            }else if(cur_line < 1000) {
+                dqh += std::__cxx11::to_string(pre_line) + ": ";
+            }
+
+            int length = token_column(token) + token_original(token).length() - 1;
+            syh += all_lines[pre_line-1].substr(0,length) + "\n";
+        }
+
+
+        dqh += all_lines[cur_line-1] + "\n";;
+
+       // xyh += "     " + token_column(token);" ";
+
+        for(int i = 0; i < token_column(token)+ 5; i++){
+            xyh += "";
+        }
+
+
+
+        return syh + dqh + xyh + "^\n";
     }
 
     // read next character if not at the end of input and update the line and column numbers
@@ -59,6 +166,39 @@ namespace Assignment_Tokeniser
         spelling += ch ;                    // remember the old ch, it is part of the current token being parsed
 
         ch = read_char() ;                  // read the next character
+        if(for_tab == '\t' && column % 4 != 0){
+            ch = ' ';
+            column++;
+            if(column % 4 ==0)
+                for_tab = '\0';
+            return;
+        }
+
+        if(ch == '\n'){
+            line++;
+            column_of_new = column++;
+            column = 1;
+        }else{
+            column++;
+        }
+
+
+
+        if(ch == '\r'){
+            char tmp = read_char();
+            if(tmp == '\n')
+                ch = tmp;
+            else
+                ch = '\n';
+
+        }
+
+
+        if(ch == '\t'){
+            for_tab = ch;
+            ch = ' ';
+
+        }
     }
 
     // initialise the tokeniser
