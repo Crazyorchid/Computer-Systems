f6e937b1353330ddbe017c6b91aaea80513d2cce
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-10-26 08:28:43 +0000

    try

--- 2020/s2/cs/assignment3/codegen.cpp
+++ 2020/s2/cs/assignment3/codegen.cpp
@@ -1,7 +1,7 @@
 #include "iobuffer.h"
 #include "symbols.h"
 #include "abstract-syntax-tree.h"
-
+#include <map>
 // to shorten our code:
 using namespace std ;
 using namespace CS_IO_Buffers ;
@@ -53,7 +53,7 @@ void walk_bool(ast t) ;
 void walk_null(ast t) ;
 void walk_this(ast t) ;
 void walk_unary_op(ast t) ;
-void walk_var(ast t) ;
+void walk_var(ast t, string push_or_pop) ;
 void walk_array_index(ast t) ;
 void walk_call_as_function(ast t) ;
 void walk_call_as_method(ast t) ;
@@ -61,14 +61,21 @@ void walk_subr_call(ast t) ;
 void walk_expr_list(ast t) ;
 void walk_infix_op(ast t) ;
 
-// walk an ast class node with fields:
-// class_name - a string
-// var_decs   - ast vector of variable declarations
-// subr_decs  - ast vector of subroutine declarations
-//
+
+#define PUSH "push"
+#define POP "pop"
+string myclassname;
+int while_label = 0;
+int if_else_label = 0;
+int if_label = 0;
+int this_counter = 0;
+void write_to_output_ln(string str){
+    write_to_output(str + "\n");
+}
+
 void walk_class(ast t)
 {
-    string myclassname = get_class_class_name(t) ;
+    myclassname = get_class_class_name(t) ;
     ast var_decs = get_class_var_decs(t) ;
     ast subr_decs = get_class_subr_decs(t) ;
 
@@ -76,9 +83,6 @@ void walk_class(ast t)
     walk_subr_decs(subr_decs) ;
 }
 
-// walk an ast class var decs node
-// it is an ast vector of variable declarations
-//
 void walk_class_var_decs(ast t)
 {
     int ndecs = size_of_class_var_decs(t) ;
@@ -88,36 +92,24 @@ void walk_class_var_decs(ast t)
     }
 }
 
-// walk an ast variable declaration with fields
-// name - a string
-// type - a string - "int", "char", "boolean" or a class name
-// segment - a string
-// offset - an int
-// this is used for statics, fields, parameters and local variables
-//
 void walk_var_dec(ast t)
 {
-    //string name = get_var_dec_name(t) ;
-    //string type = get_var_dec_type(t) ;
-    //string segment = get_var_dec_segment(t) ;
-    //int offset = get_var_dec_offset(t) ;
+    string segment = get_var_dec_segment(t) ;
+    if (segment == "this")
+        this_counter = this_counter + 1;
 }
 
-// walk an ast class var decs node
-// it is an ast vector of subroutine declarations
-//
 void walk_subr_decs(ast t)
 {
     int size = size_of_subr_decs(t) ;
     for ( int i = 0 ; i < size ; i++ )
     {
+        while_label = 0;
+        if_label = 0;
         walk_subr(get_subr_decs(t,i)) ;
     }
 }
 
-// walk an ast subroutine node with a single field
-// subr - an ast constructor, ast function or ast method node
-//
 void walk_subr(ast t)
 {
     ast subr = get_subr_subr(t) ;
@@ -139,60 +131,50 @@ void walk_subr(ast t)
     }
 }
 
-// walk an ast constructor node with fields
-// vtype - a string - the name of the class
-// name - a string
-// param list - an ast vector of variable declarations
-// subr body - an ast subr body node
-//
 void walk_constructor(ast t)
 {
-    //string vtype = get_constructor_vtype(t) ;
-    //string name = get_constructor_name(t) ;
+    string vtype = get_constructor_vtype(t) ;
+    string name = get_constructor_name(t) ;
     ast param_list = get_constructor_param_list(t) ;
     ast subr_body = get_constructor_subr_body(t) ;
 
     walk_param_list(param_list) ;
+    write_to_output_ln("function "+myclassname+"."+name+" "+
+    to_string(size_of_var_decs(get_subr_body_decs(subr_body))));
+    write_to_output_ln("push constant "+to_string(this_counter));
+    write_to_output_ln("call Memory.alloc 1");
+    write_to_output_ln("pop pointer 0");
     walk_subr_body(subr_body) ;
 }
 
-// walk an ast function node with fields
-// vtype - a string - one of "void", "int", "char", "boolean" or the name of a class
-// name - a string
-// param list - an ast vector of variable declarations
-// subr body - an ast subr body node
-//
 void walk_function(ast t)
 {
-    //string vtype = get_function_vtype(t) ;
-    //string name = get_function_name(t) ;
+    string vtype = get_function_vtype(t) ;
+    string name = get_function_name(t) ;
     ast param_list = get_function_param_list(t) ;
     ast subr_body = get_function_subr_body(t) ;
 
     walk_param_list(param_list) ;
+    write_to_output_ln("function "+myclassname+"."+name+" "+
+    to_string(size_of_var_decs(get_subr_body_decs(subr_body))));
     walk_subr_body(subr_body) ;
 }
 
-// walk an ast method node with fields
-// vtype - a string - one of "void", "int", "char", "boolean" or the name of a class
-// name - a string
-// param list - an ast vector of variable declarations
-// subr body - an ast subr body node
-//
 void walk_method(ast t)
 {
-    //string vtype = get_method_vtype(t) ;
-    //string name = get_method_name(t) ;
+    string vtype = get_method_vtype(t) ;
+    string name = get_method_name(t) ;
     ast param_list = get_method_param_list(t) ;
     ast subr_body = get_method_subr_body(t) ;
 
     walk_param_list(param_list) ;
+    write_to_output_ln("function "+myclassname+"."+name+" "+
+    to_string(size_of_var_decs(get_subr_body_decs(subr_body))));
+    write_to_output_ln("push argument 0");
+    write_to_output_ln("pop pointer 0");
     walk_subr_body(subr_body) ;
 }
 
-// walk an ast param list node
-// it is an ast vector of variable declarations
-//
 void walk_param_list(ast t)
 {
     int ndecs = size_of_param_list(t) ;
@@ -202,10 +184,6 @@ void walk_param_list(ast t)
     }
 }
 
-// walk an ast subr body node with fields
-// decs - an ast vector of variable declarations
-// body - an ast vector of statement nodes
-//
 void walk_subr_body(ast t)
 {
     ast decs = get_subr_body_decs(t) ;
@@ -215,9 +193,6 @@ void walk_subr_body(ast t)
     walk_statements(body) ;
 }
 
-// walk an ast param list node
-// it is an ast vector of variable declarations
-//
 void walk_var_decs(ast t)
 {
     int ndecs = size_of_var_decs(t) ;
@@ -227,9 +202,6 @@ void walk_var_decs(ast t)
     }
 }
 
-// walk an ast statements node
-// it is an ast vector of statement nodes
-//
 void walk_statements(ast t)
 {
     int nstatements = size_of_statements(t) ;
@@ -239,9 +211,6 @@ void walk_statements(ast t)
     }
 }
 
-// walk an ast statement node with a single field
-// statement - one of the following ast nodes, let, let array, if, if else, while, do, return, return expr or statements
-//
 void walk_statement(ast t)
 {
     ast statement = get_statement_statement(t) ;
@@ -281,80 +250,79 @@ void walk_statement(ast t)
     }
 }
 
-// walk an ast let node with fields
-// var  - an ast variable
-// expr - an ast expr node
-//
 void walk_let(ast t)
 {
     ast var = get_let_var(t) ;
     ast expr = get_let_expr(t) ;
 
-    walk_var(var) ;
     walk_expr(expr) ;
+    walk_var(var, POP) ;
 }
 
-// walk an ast let array node with fields
-// var    - an ast variable
-// index  - an ast expr node
-// expr   - an ast expr node
-//
 void walk_let_array(ast t)
 {
     ast var = get_let_array_var(t) ;
     ast index = get_let_array_index(t) ;
     ast expr = get_let_array_expr(t) ;
 
-    walk_var(var) ;
     walk_expr(index) ;
+    walk_var(var, PUSH) ;
+    write_to_output_ln("add");
     walk_expr(expr) ;
+    write_to_output_ln("pop temp 0");
+    write_to_output_ln("pop pointer 1");
+    write_to_output_ln("push temp 0");
+    write_to_output_ln("pop that 0");
 }
 
-// walk an ast if node with fields
-// condition - an ast expr node
-// if true   - an ast statements node
-//
 void walk_if(ast t)
 {
     ast condition = get_if_condition(t) ;
     ast if_true = get_if_if_true(t) ;
 
+    string label_idx = to_string(if_label++);
     walk_expr(condition) ;
+    write_to_output_ln("if-goto IF_TRUE" + label_idx);
+    write_to_output_ln("goto IF_FALSE" + label_idx);
+    write_to_output_ln("label IF_TRUE" + label_idx);
     walk_statements(if_true) ;
+    write_to_output_ln("label IF_FALSE" + label_idx);
 }
 
-// walk an ast if else node with fields
-// condition - an ast expr node
-// if true   - an ast statements node
-// if else   - an ast statements node
-//
 void walk_if_else(ast t)
 {
     ast condition = get_if_else_condition(t) ;
     ast if_true = get_if_else_if_true(t) ;
     ast if_false = get_if_else_if_false(t) ;
 
+    string label_idx = to_string(if_label++);
     walk_expr(condition) ;
+    write_to_output_ln("if-goto IF_TRUE" + label_idx) ;
+    write_to_output_ln("goto IF_FALSE" + label_idx) ;
+    write_to_output_ln("label IF_TRUE" + label_idx) ;
+
     walk_statements(if_true) ;
+    write_to_output_ln("goto IF_END" + label_idx) ;
+    write_to_output_ln("label IF_FALSE" + label_idx) ;
     walk_statements(if_false) ;
+    write_to_output_ln("label IF_END" + label_idx) ;
 }
 
-// walk an ast while node with fields
-// condition - an ast expr node
-// body      - an ast statements node
-//
 void walk_while(ast t)
 {
     ast condition = get_while_condition(t) ;
     ast body = get_while_body(t) ;
-
+    
+    string label_idx = to_string(while_label++);
+    write_to_output_ln("label WHILE_EXP"+label_idx);
     walk_expr(condition) ;
+    write_to_output_ln("not");
+    write_to_output_ln("if-goto WHILE_END"+label_idx);
     walk_statements(body) ;
+    write_to_output_ln("goto WHILE_EXP"+label_idx);
+    write_to_output_ln("label WHILE_END"+label_idx);
 }
 
-// walk an ast do node with a single field
-// call - an ast call as function node or an ast call as method node
-//
 void walk_do(ast t)
 {
     ast call = get_do_call(t) ;
@@ -371,52 +339,50 @@ void walk_do(ast t)
         fatal_error(0,"Unexpected call kind") ;
         break ;
     }
+    write_to_output_ln("pop temp 0");
 }
 
-// walk an ast return node, it has not fields
-//
 void walk_return(ast t)
 {
+    write_to_output_ln("push constant 0");
+    write_to_output_ln("return");
 }
 
-// walk an ast return expr node with a single field
-// expr - an ast expr node
-//
 void walk_return_expr(ast t)
 {
     ast expr = get_return_expr(t) ;
 
     walk_expr(expr) ;
+    write_to_output_ln("return");
 }
 
-// walk an ast param list node
-// it is an ast vector of ast term and infix op nodes
-// it must be of odd length > 0, ie 1, 3, 5, 7, ...
-// all elements at even indices are an ast term node
-// all elements at odd indices are an ast infix op
-//
+map<string, string> op_map = 
+{
+    {"+", "add"},
+    {"-", "sub"},
+    {"*", "call Math.multiply 2"},
+    {"/", "call Math.divide 2"},
+    {"&", "and"},
+    {"|", "or"},
+    {">", "gt"},
+    {"=", "eq"},
+    {"<", "lt"}
+};
 void walk_expr(ast t)
 {
     int term_ops = size_of_expr(t) ;
-    for ( int i = 0 ; i < term_ops ; i++ )
+    for ( int i = 0 ; i < term_ops ; i+=2 )
     {
         ast term_op = get_expr(t,i) ;
-        if ( i % 2 == 0 )
-        {
-            walk_term(term_op) ;
-        }
-        else
-        {
-            walk_infix_op(term_op) ;
-        }
+        walk_term(term_op) ;
+    }
+    for ( int i = ((term_ops - 1 & 1) == 1 ? term_ops - 1 : term_ops - 2); i > 0 ; i-=2 )
+    {
+        ast term_op = get_expr(t,i) ;
+        write_to_output_ln( op_map[get_infix_op_op(term_op)] );
     }
 }
 
-// walk an ast term node with a single field
-// term - one of the following ast nodes:
-//        int, string, bool, null, this, expr, unary op,
-//        variable, array index, call as function or call as method
-//
 void walk_term(ast t)
 {
     ast term = get_term_term(t) ;
@@ -445,7 +411,7 @@ void walk_term(ast t)
         walk_unary_op(term) ;
         break ;
     case ast_var:
-        walk_var(term) ;
+        walk_var(term, PUSH) ;
         break ;
     case ast_array_index:
         walk_array_index(term) ;
@@ -462,100 +428,83 @@ void walk_term(ast t)
     }
 }
 
-// walk an ast int node with a single field
-// constant - an integer in the range -32,768 to 32,767
-//
 void walk_int(ast t)
 {
-    //int _constant = get_int_constant(t) ;
+    int _constant = get_int_constant(t) ;
+    write_to_output_ln("push constant "+to_string(_constant));
 }
 
-// walk an ast string node with a single field
-// constant - a string
-//
 void walk_string(ast t)
 {
-    //string _constant = get_string_constant(t) ;
+    string _constant = get_string_constant(t) ;
+    write_to_output_ln("push constant "+to_string(_constant.size()));
+    write_to_output_ln("call String.new 1");
+    for(int i = 0; i < _constant.size(); i++){
+        string s = to_string( (int)_constant[i] );
+        write_to_output_ln("push constant "+ s);
+        write_to_output_ln("call String.appendChar 2");
+    }
 }
 
-// walk an ast bool node with a single field
-// constant - either true or false
-//
 void walk_bool(ast t)
 {
-    //bool _constant = get_bool_t_or_f(t) ;
+    bool _constant = get_bool_t_or_f(t) ;
+    string r = _constant ? "\nnot" : "" ;
+    write_to_output_ln("push constant 0" + r);
 }
 
-// walk an ast null node, it has not fields
-//
 void walk_null(ast t)
 {
+    write_to_output_ln("push constant 0");
 }
 
-// walk an ast this node, it has not fields
-//
 void walk_this(ast t)
 {
+    write_to_output_ln("push pointer 0");
 }
 
-// walk an ast unary op node with fields
-// op   - a string, either "-" or "~"
-// term - an ast term node
-//        int, string, bool, null, this, expr, unary op,
-//        variable, array index, call as function or call as method
-//
 void walk_unary_op(ast t)
 {
     string uop = get_unary_op_op(t);
     ast term = get_unary_op_term(t) ;
 
     walk_term(term) ;
+    write_to_output_ln(uop == "~" ? "not" : "neg");
 }
 
-// walk an ast variable node with fields
-// name - a string
-// type - a string - "int", "char", "boolean" or a class name
-// segment - a string
-// offset - an int
-//
-void walk_var(ast t)
+void walk_var(ast t, string push_or_pop)
 {
-    //string name = get_var_name(t) ;
-    //string type = get_var_type(t) ;
-    //string segment = get_var_segment(t) ;
-    //int offset = get_var_offset(t) ;
+    string name = get_var_name(t) ;
+    string type = get_var_type(t) ;
+    string segment = get_var_segment(t) ;
+    string offset = to_string(get_var_offset(t)) ;
+
+    write_to_output_ln(push_or_pop + " " + segment+" "+ offset);
 }
 
-// walk an ast array index node with fields
-// var   - an ast variable node
-// index - an ast expr node
-//
 void walk_array_index(ast t)
 {
     ast var = get_array_index_var(t) ;
     ast index = get_array_index_index(t) ;
 
-    walk_var(var) ;
     walk_expr(index) ;
+    walk_var(var, PUSH) ;
+    write_to_output_ln("add");
+    write_to_output_ln("pop pointer 1");
+    write_to_output_ln("push that 0");
 }
 
-// walk an ast subr call as method with fields
-// class name - a string
-// call       - an ast subr call node
-//
 void walk_call_as_function(ast t)
 {
     string class_name = get_call_as_function_class_name(t) ;
     ast subr_call = get_call_as_function_subr_call(t) ;
 
-    walk_subr_call(subr_call) ;
+    string subr_name = get_subr_call_subr_name(subr_call) ;
+    ast expr_list = get_subr_call_expr_list(subr_call) ;
+    walk_expr_list(expr_list) ;
+    write_to_output_ln("call "+class_name+"."+subr_name+" "+to_string(size_of_expr_list(expr_list)));
 }
 
-// walk an ast subr call as method with fields
-// class name - a string
-// var        - an ast variable or ast this node, hidden first parameter of the call
-// call       - an ast subr call node
-//
 void walk_call_as_method(ast t)
 {
     string class_name = get_call_as_method_class_name(t) ;
@@ -568,30 +517,20 @@ void walk_call_as_method(ast t)
         walk_this(var) ;
         break ;
     case ast_var:
-        walk_var(var) ;
+        walk_var(var, PUSH) ;
         break ;
     default:
         fatal_error(0,"Expected var or this") ;
         break ;
     }
-    walk_subr_call(subr_call) ;
-}
-
-// walk an ast subr call node with fields
-// name      - a string
-// expr list - a vector of ast expr nodes
-//
-void walk_subr_call(ast t)
-{
-    string subr_name = get_subr_call_subr_name(t) ;
-    ast expr_list = get_subr_call_expr_list(t) ;
-
+    string subr_name = get_subr_call_subr_name(subr_call) ;
+    ast expr_list = get_subr_call_expr_list(subr_call) ;
     walk_expr_list(expr_list) ;
+    write_to_output_ln("call "+class_name+"."+subr_name+" "+to_string(size_of_expr_list(expr_list) + 1));
 }
 
-// walk an ast expr list node
-// it is an ast vector of ast expr nodes
-//
+
+
 void walk_expr_list(ast t)
 {
     int nexpressions = size_of_expr_list(t) ;
@@ -601,13 +540,6 @@ void walk_expr_list(ast t)
     }
 }
 
-// walk an ast infix op node with a single field
-// op - a string - one of "+", "-", "*", "/", "&", "|", "<", ">", "="
-//
-void walk_infix_op(ast t)
-{
-    //string op = get_infix_op_op(t) ;
-}
 
 // main program
 int main(int argc,char **argv)
